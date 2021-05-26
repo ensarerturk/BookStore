6bb461b8e5bc8de8cbc5d9095858981a252b734c
ensar <ensarerturk15@gmail.com>
2021-05-26 19:33:20 +0300

    First Commit

---
+++ Bookstore/src/net/codejava/javaee/bookstore/BookDAO.java
@@ -0,0 +1,151 @@
+package net.codejava.javaee.bookstore;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * veritabanýndaki tablo kitabý için CRUD iþlemleri saðlayan bir Veri Eriþim Katmaný 
+ * sýnýfý uygulamamýz gerekiyor
+
+ */
+public class BookDAO {
+	private String jdbcURL;
+	private String jdbcUsername;
+	private String jdbcPassword;
+	private Connection jdbcConnection;
+	
+	public BookDAO(String jdbcURL, String jdbcUsername, String jdbcPassword) {
+		this.jdbcURL = jdbcURL;
+		this.jdbcUsername = jdbcUsername;
+		this.jdbcPassword = jdbcPassword;
+	}
+	
+	protected void connect() throws SQLException {
+		if (jdbcConnection == null || jdbcConnection.isClosed()) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				throw new SQLException(e);
+			}
+			jdbcConnection = DriverManager.getConnection(
+										jdbcURL, jdbcUsername, jdbcPassword);
+		}
+	}
+	
+	protected void disconnect() throws SQLException {
+		if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+			jdbcConnection.close();
+		}
+	}
+	
+	/* insertBook - bu, tablo kitabýna yeni bir satýr ekler . */
+	public boolean insertBook(Book book) throws SQLException {
+		String sql = "INSERT INTO book (title, author, price) VALUES (?, ?, ?)";
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setString(1, book.getTitle());
+		statement.setString(2, book.getAuthor());
+		statement.setFloat(3, book.getPrice());
+		
+		boolean rowInserted = statement.executeUpdate() > 0;
+		statement.close();
+		disconnect();
+		return rowInserted;
+	}
+	
+	/* listAllBooks () - bu, tüm satýrlarý alýr; ve getBook (id) - birincil anahtar deðerine (ID)
+	 *  göre belirli bir satýrý döndürür */
+	public List<Book> listAllBooks() throws SQLException {
+		List<Book> listBook = new ArrayList<>();
+		
+		String sql = "SELECT * FROM book";
+		
+		connect();
+		
+		Statement statement = jdbcConnection.createStatement();
+		ResultSet resultSet = statement.executeQuery(sql);
+		
+		while (resultSet.next()) {
+			int id = resultSet.getInt("book_id");
+			String title = resultSet.getString("title");
+			String author = resultSet.getString("author");
+			float price = resultSet.getFloat("price");
+			
+			Book book = new Book(id, title, author, price);
+			listBook.add(book);
+		}
+		
+		resultSet.close();
+		statement.close();
+		
+		disconnect();
+		
+		return listBook;
+	}
+	
+	/* deleteBook - bu, birincil anahtar deðerine (ID) dayalý 
+	 * olarak veritabanýndaki mevcut bir satýrý kaldýrýr. */
+	public boolean deleteBook(Book book) throws SQLException {
+		String sql = "DELETE FROM book where book_id = ?";
+		
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setInt(1, book.getId());
+		
+		boolean rowDeleted = statement.executeUpdate() > 0;
+		statement.close();
+		disconnect();
+		return rowDeleted;		
+	}
+	
+	/* updateBook (Kitap) - bu, veritabanýndaki mevcut bir satýrý günceller. */
+	public boolean updateBook(Book book) throws SQLException {
+		String sql = "UPDATE book SET title = ?, author = ?, price = ?";
+		sql += " WHERE book_id = ?";
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setString(1, book.getTitle());
+		statement.setString(2, book.getAuthor());
+		statement.setFloat(3, book.getPrice());
+		statement.setInt(4, book.getId());
+		
+		boolean rowUpdated = statement.executeUpdate() > 0;
+		statement.close();
+		disconnect();
+		return rowUpdated;		
+	}
+	
+	public Book getBook(int id) throws SQLException {
+		Book book = null;
+		String sql = "SELECT * FROM book WHERE book_id = ?";
+		
+		connect();
+		
+		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+		statement.setInt(1, id);
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		if (resultSet.next()) {
+			String title = resultSet.getString("title");
+			String author = resultSet.getString("author");
+			float price = resultSet.getFloat("price");
+			
+			book = new Book(id, title, author, price);
+		}
+		
+		resultSet.close();
+		statement.close();
+		
+		return book;
+	}
+}
